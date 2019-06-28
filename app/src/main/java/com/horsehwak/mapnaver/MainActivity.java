535bad439afdfe837ec1c1945528982d51352a5e
HorseHwak <rhkralstn91@naver.com>
2019-06-28 17:37:14 +0900

    Marker

--- app/src/main/java/com/horsehwak/mapnaver/MainActivity.java
+++ app/src/main/java/com/horsehwak/mapnaver/MainActivity.java
@@ -1,14 +1,49 @@
 package com.horsehwak.mapnaver;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.UiThread;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.CameraAnimation;
+import com.naver.maps.map.CameraUpdate;
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.Marker;
 
+public  class MainActivity extends FragmentActivity
+    implements OnMapReadyCallback{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        MapFragment mapFragment = (MapFragment) fm.findFragmentById(R.id.map);
+        if (mapFragment == null) {
+            mapFragment = MapFragment.newInstance();
+            fm.beginTransaction().add(R.id.map, mapFragment).commit();
+        }
+        mapFragment.getMapAsync(this);
+    }
+    @UiThread
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        CameraUpdate cameraUpdate = CameraUpdate.scrollTo(new LatLng(35.945357, 126.682163))
+        .animate(CameraAnimation.Easing);
+        naverMap.moveCamera(cameraUpdate);
+
+        Marker marker = new Marker();
+        marker.setPosition(new LatLng(35.945357, 126.682163));
+        marker.setMap(naverMap);
+
     }
 }
+
